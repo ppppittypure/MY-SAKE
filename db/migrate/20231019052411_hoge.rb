74db1fab1a57d5be3061f6da72929807c4a29689
mai kondo <144329363+ppppittypure@users.noreply.github.com>
2023-11-28 18:13:50 +0900

    c

--- db/migrate/20231019052411_hoge.rb
+++
@@ -1,9 +0,0 @@
-class Hoges < ActiveRecord::Migration[7.0]
-  def change
-    create_table :hoges do |t|
-
-      t.timestamps
-    end
-  end
-end
-
--- db/schema.rb
+++ db/schema.rb
@@ -54,16 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_26_051821) do
     t.index ["user_id"], name: "index_rankings_on_user_id"
   end
 
-  create_table "sakes", charset: "utf8mb4", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "text", null: false
-    t.float "evaluation", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sakes_on_user_id"
-  end
-
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,5 +74,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_26_051821) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "rankings", "users"
-  add_foreign_key "sakes", "users"
 end
